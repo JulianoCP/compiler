dd3c6066de3c063a0c786f553ec334f60853f2d1
julianocp <julianopetini@alunos.utfpr.edu.br>
2020-03-11 12:54:51 -0300

    core

--- main.py
+++ main.py
@@ -1,45 +1,65 @@
-# ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
-# ------------------------------------------------------------
 import ply.lex as lex
+import tokens
 
-# List of token names.   This is always required
-tokens = (
-'NUMBER',
-'PLUS',
-'MINUS',
-'TIMES',
-'DIVIDE',
-'LPAREN',
-'RPAREN',
-)
+tokens = tokens.Toke()
 
-# Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_SE = r'se'
+t_FIM = r'fim'
+t_ATE = r'até'
+t_LEIA = r'leia'
+t_MAIS = r'\+'
+t_MENOS = r'-'
+t_MENOR = r'<'
+t_MAIOR = r'>'
+t_IGUAL = r'='
+t_ENTAO = r'então'
+t_REPITA = r'repita'
+t_DIVISAO = r'/'
+t_VIRGULA = r','
+t_NEGACAO = r'!'
+t_ESCREVA = r'escreva'
+t_RETORNA = r'retorna'
+t_INTEIRO = r'inteiro'
+t_E_LOGICO = r'&&'
+t_FLUTUANTE = r'flutuante'
+t_DIFERENTE = r'<>'
+t_OU_LOGICO = r'\|\|'
+t_ATRIBUICAO = r':='
+t_DOIS_PONTOS = r':'
+t_MENOR_IGUAL = r'<='
+t_MAIOR_IGUAL = r'>='
+t_ABRE_COLCHETE = r'\['
+t_MULTIPLICACAO = r'\*'
+t_FECHA_COLCHETE = r'\]'
+t_ABRE_PARENTESE = r'\('
+t_FECHA_PARENTESE = r'\)'
 
-# A regular expression rule with some action code
-def t_NUMBER(t):
+def t_NUM_NOTACAO_CIENTIFICA(t):
+    r'\d+\^+\d+'
+    t.value = t.value    
+    return t
+
+def t_NUM_PONTO_FLUTUANTE(t):
+    r'\d+\.\d+' 
+    t.value = float(t.value)    
+    return t
+
+def t_NUM_INTEIRO(t):
     r'\d+'
     t.value = int(t.value)    
     return t
 
-# Define a rule so we can track line numbers
+def t_ID(t):
+    r'[aA-zZ]+'
+    t.value = str(t.value)    
+    return t
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
 t_ignore  = ' \t'
 
-# Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
@@ -47,16 +67,22 @@ def t_error(t):
 # Build the lexer
 lexer = lex.lex()
 
+def tokenize(data):
 
-data = '''
- 3 + 4 * 10
-   + -20 *2
- '''
+    lexer.input(data)
 
-lexer.input(data)
+    while True:
+        tok = lexer.token()
+        if not tok: 
+            break      
+        print(tok.type, tok.value)
 
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok)
+def main():
+    
+    f = open("input.tpp","r")
+    data = str(f.read())
+
+    tokenize(data)
+
+if __name__ == '__main__':
+    main()
---
+++ tokens.py
@@ -0,0 +1,42 @@
+
+def Toke():
+    tokens = (
+    'MAIS',
+    'MENOS',
+    'MULTIPLICACAO',
+    'DIVISAO',
+    'DOIS_PONTOS',
+    'VIRGULA',
+    'MENOR',
+    'MAIOR',
+    'IGUAL',
+    'DIFERENTE',
+    'MENOR_IGUAL',
+    'MAIOR_IGUAL',
+    'E_LOGICO',
+    'OU_LOGICO',
+    'NEGACAO',
+    'ABRE_PARENTESE',
+    'FECHA_PARENTESE',
+    'ABRE_COLCHETE',
+    'FECHA_COLCHETE',
+    'SE',
+    'ENTAO',
+    'SENAO',
+    'FIM',
+    'REPITA',
+    'ATE',
+    'ATRIBUICAO',
+    'LEIA',
+    'ESCREVA',
+    'RETORNA',
+    'INTEIRO',
+    'FLUTUANTE',
+    'NUM_INTEIRO',
+    'NUM_PONTO_FLUTUANTE',
+    'NUM_NOTACAO_CIENTIFICA',
+    'ID',
+    )
+
+    return tokens
+
