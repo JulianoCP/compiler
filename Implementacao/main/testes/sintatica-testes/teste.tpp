a0f63d24eb485c8351b8bad58fed0c9c27af5dc9
JulianoCP <julianopetini@alunos.utfpr.edu.br>
2021-05-05 10:33:17 -0300

    removendo versão

--- Implementacao/main/testes/sintatica-testes/teste.tpp
+++
@@ -1,4 +0,0 @@
-inteiro principal(inteiro: n)
-  temp := 1+1
-  retorna(0)
-fim
--- Implementacao/vsCode/transfira.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-###---LINUX---###
-#rm -r ~/.vscode/extensions/tpp/
-cp -r tpp/ ~/.vscode/extensions/tpp/
-cp -r tpptheme/ ~/.vscode/extensions/tpptheme/
-###-----------------------------------------------###
